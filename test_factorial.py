e740cf14bc97a2e9ba68ed38375f36e8fe95ba1d
Prashantism12 <pashu.s.madival@gmail.com>
2026-01-18 16:36:51 +0530

    Update test_factorial.py

--- test_factorial.py
+++ test_factorial.py
@@ -1,6 +1,12 @@
 from factorial import find
-def test_find():
-    assert find(3)==6
-def test_find():
-    assert  find(5)==120
 
+def test_case1():
+    assert find(3) == 6
+
+def test_case2():
+    assert find(5) == 120
+
+if __name__ == "__main__":
+    test_case1()
+    test_case2()
+    
