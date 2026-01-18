b0526984aeed5d56d792878d64f72f4706710c0f
Prashantism12 <pashu.s.madival@gmail.com>
2026-01-18 16:18:31 +0530

    save

---
+++ factorial.py
@@ -0,0 +1,13 @@
+import math
+def find(n):
+    return math.factorial(n)
+
+if __name__ == "__main__":
+     n=5
+     result=find(n)
+     print("result",result)
+
+    
+
+
+
---
+++ test_factorial.py
@@ -0,0 +1,6 @@
+from factorial import find
+def test_case1():
+    assert find(3)==6
+def test_case2():
+    assert  find(5)==120
+
