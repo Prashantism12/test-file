86a9e457fe1961caa2f9144b8799c12339d25278
Prashantism12 <pashu.s.madival@gmail.com>
2026-01-18 16:38:37 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,8 +3,8 @@ def find(n):
     return math.factorial(n)
 
 if __name__ == "__main__":
-     
-     result=find(n)
+    n=int(input("enter number"))
+    result=find(n)
      print("result",result)
 
     
